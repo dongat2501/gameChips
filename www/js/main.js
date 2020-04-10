994c4c8795a62b3669a5ebd6cc11d873784ba9a4
viettran <viet.tranhoang@powergatesoftware.com>
2020-04-10 11:58:37 +0700

    backbtn

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     $('.btn-router').click(function (e) {
-        console.log(e)
         var url = $(this).attr('url');
         window.location.pathname = '/' + url;
-        
 
+
+    })
+    $('#btn-back').click(function (e) {
+        window.history.back();
     })
 });
